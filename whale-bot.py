da50d6d9c00555a0c55bd0f0e5d6a3133970b5a0
Matt Martin <matt.martin@hey.com>
2022-04-21 10:19:12 -0700

    changed date formatting on timestamp

--- whale-bot.py
+++ whale-bot.py
@@ -38,7 +38,7 @@ else:
 
         # add fields to embed
         embed.add_embed_field(name='Transaction ID', value=whale['txSignature'], inline=False)
-        embed.add_embed_field(name='Time', value=whale['timeStamp'], inline=False)
+        embed.add_embed_field(name='Time', value=datetime.strptime(whale['timestamp'],"%Y-%m-%dT%H:%M:%SZ").strftime('%Y-%m-%d %H:%M:%S'), inline=False)
         embed.add_embed_field(name='Whale', value=whale['userAddress'], inline=False)
         embed.add_embed_field(name='Product', value=whale['product_name'],inline=False)
         embed.add_embed_field(name='Volt', value=str(whale['volt_number']))
